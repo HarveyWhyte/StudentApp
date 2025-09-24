985b2c152afb1fdd843e852f37a4b0baa2858228
HARVEY WHYTE <0whyth98@solent.ac.uk>
2025-09-24 16:18:09 +0100

    Student functionality

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val id = 0
+    var loop = true
+    var students = arrayListOf<Student>()
+    while (loop) {
+        println("Enter Name: ")
+        val name = readln()
+        println("Enter Course: ")
+        val course = readln()
+        println("Enter Mark: ")
+        val mark = readln().toDouble()
+        val student = Student(id.toString(), name, course, mark)
+        students.add(student)
+        for (student in students) {
+            println(student.toString())
+        }
+    }
+}
